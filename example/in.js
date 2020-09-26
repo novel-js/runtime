aa22a03028ec8980657fd795e69817e2b5b4fea3
Rusty Shackleford <a@neb.best>
2020-09-26 15:14:31 -0400

    Try to add clippy action

--- example/in.js
+++ example/in.js
@@ -1,8 +1,20 @@
 import {println} from "https://novel-js.github.io/pkgs/std/std/println.js";
 
 import std from "https://novel-js.github.io/pkgs/std/std/dist.js";
-std.file.write("test.txt", "test file")
-std.file.append("test.txt", "A new line!");
-std.file.exists("test.txt").then(exi => {
-    std.print(exi)
-})
+(async function(){
+    std.file.write("test.txt", "test file")
+    std.file.append("test.txt", "A new line!");
+    // std.file.exists("test.txt").then(exi => {
+    //     std.print(exi)
+    // })
+    let exi = await std.file.exists("test.txt");
+    std.println(exi)
+    let a = {
+        a: "Letter a",
+    }
+    let b = {
+        "c": a
+    }
+    std.println(b)
+})()
+// import { print } from "../pkgs/buckets/std/projects/std/dist";
