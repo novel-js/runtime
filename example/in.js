1e785fc55cf879e5ee7aabe06c7fd0c33352b11a
Rusty Shackleford <a@neb.best>
2020-09-26 12:53:47 -0400

    Update example to remove old tests

--- example/in.js
+++ example/in.js
@@ -1,15 +1,4 @@
 import {println} from "https://novel-js.github.io/pkgs/std/std/println.js";
 
 import std from "https://novel-js.github.io/pkgs/std/std/dist.js";
-// import { println } from "../pkgs/buckets/std/projects/std/dist";
-// println("a")
-// std
-println("")
-println(std.file.read)
-// file.read("example/test.txt").then(content => {
-//     print("content", content)
-// }).catch(err => {
-//     print("err", err)
-// })
-// import {read} from "http://pkgs.io:3030/pkg/std/file";
-// println(read)
+std.print($)
