c9f67951543450a817df29b0ed0225c7f21a3d48
Rusty Shackleford <a@neb.best>
2020-09-26 15:24:27 -0400

    Collapse match into if statements

--- src/kfs.rs
+++ src/kfs.rs
@@ -20,20 +20,16 @@ pub fn write(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,mu
     let file_name = args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope);
     let content = args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope);
     let file_handle = fs::write(file_name, content);
-    match file_handle{
-        Ok(f) => {
-            ret.set(v8::undefined(scope).into());
-        }
-        Err(e) => {
-            let msg = v8::String::new(scope, &e.to_string()).unwrap();
-            scope.throw_exception(msg.into());
-        }
-    }    
+    if file_handle.is_err(){
+        let msg = v8::String::new(scope, &e.to_string()).unwrap();
+        scope.throw_exception(msg.into());
+    }
+    ret.set(v8::undefined(scope).into());
 }
 pub fn append(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,mut ret:  v8::ReturnValue,){
     let file_name = args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope);
     let content = args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope);
-    let mut file_handle = std::fs::OpenOptions::new()
+    let file_handle = std::fs::OpenOptions::new()
     .write(true)
     .append(true)
     .open(file_name);
