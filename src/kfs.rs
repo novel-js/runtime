2fc745c91b7f458ea88318a432d520cb7c01c340
Rusty Shackleford <a@neb.best>
2020-09-26 12:51:54 -0400

    Add $.fs_write

--- src/kfs.rs
+++ src/kfs.rs
@@ -11,10 +11,24 @@ pub fn read(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,mut
         }
         Err(e) => {
             let msg = v8::String::new(scope, &e.to_string()).unwrap();
-            // let exc = v8::Exception::error(scope, msg.into());
             scope.throw_exception(msg.into());
-            // ret.set(exc);
+        }
+    }    
+}
+
+pub fn write(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,mut ret:  v8::ReturnValue,){
+    let file_name = args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope);
+    let content = args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope);
+    let file_handle = fs::write(file_name, content);
+    match file_handle{
+        Ok(f) => {
+            ret.set(v8::undefined(scope).into());
             // ret.set(v8::String::new(scope, &String::from_utf8(f).unwrap()).unwrap().into())
+            
+        }
+        Err(e) => {
+            let msg = v8::String::new(scope, &e.to_string()).unwrap();
+            scope.throw_exception(msg.into());
         }
     }    
 }
