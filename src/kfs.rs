a5a4ca38ef557085a3d75d10123959f364524bbc
Rusty Shackleford <a@neb.best>
2020-09-26 21:57:10 -0400

    Better formattting

--- src/kfs.rs
+++ src/kfs.rs
@@ -1,43 +1,77 @@
 use rusty_v8 as v8;
 use std::fs;
 use std::io::prelude::*;
-pub fn read(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,mut ret:  v8::ReturnValue,){
+pub fn read(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    mut ret: v8::ReturnValue,
+) {
     let file_name = args.get(0);
-    let file_handle = fs::read(file_name.to_string(scope).unwrap().to_rust_string_lossy(scope));
-    match file_handle{
-        Ok(f) => {
-            ret.set(v8::String::new(scope, &String::from_utf8(f).unwrap()).unwrap().into())
-   
-        }
+    let file_handle = fs::read(
+        file_name
+            .to_string(scope)
+            .unwrap()
+            .to_rust_string_lossy(scope),
+    );
+    match file_handle {
+        Ok(f) => ret.set(
+            v8::String::new(scope, &String::from_utf8(f).unwrap())
+                .unwrap()
+                .into(),
+        ),
         Err(e) => {
             let msg = v8::String::new(scope, &e.to_string()).unwrap();
             scope.throw_exception(msg.into());
         }
-    }    
+    }
 }
 
-pub fn write(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,mut ret:  v8::ReturnValue,){
-    let file_name = args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope);
-    let content = args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope);
+pub fn write(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    mut ret: v8::ReturnValue,
+) {
+    let file_name = args
+        .get(0)
+        .to_string(scope)
+        .unwrap()
+        .to_rust_string_lossy(scope);
+    let content = args
+        .get(1)
+        .to_string(scope)
+        .unwrap()
+        .to_rust_string_lossy(scope);
     let file_handle = fs::write(file_name, content);
-    if let Err(e) = file_handle{
+    if let Err(e) = file_handle {
         let msg = v8::String::new(scope, &e.to_string()).unwrap();
         scope.throw_exception(msg.into());
     }
     ret.set(v8::undefined(scope).into());
 }
-pub fn append(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,mut ret:  v8::ReturnValue,){
-    let file_name = args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope);
-    let content = args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope);
+pub fn append(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    mut ret: v8::ReturnValue,
+) {
+    let file_name = args
+        .get(0)
+        .to_string(scope)
+        .unwrap()
+        .to_rust_string_lossy(scope);
+    let content = args
+        .get(1)
+        .to_string(scope)
+        .unwrap()
+        .to_rust_string_lossy(scope);
     let file_handle = std::fs::OpenOptions::new()
-    .write(true)
-    .append(true)
-    .open(file_name);
-    
+        .write(true)
+        .append(true)
+        .open(file_name);
+
     // let file_handle = fs::app(file_name, content);
-    match file_handle{
+    match file_handle {
         Ok(mut f) => {
-            match writeln!(f, "{}", content){
+            match writeln!(f, "{}", content) {
                 Ok(_) => {
                     ret.set(v8::undefined(scope).into());
                 }
@@ -46,11 +80,10 @@ pub fn append(scope: &mut v8::HandleScope, args: v8::FunctionCallbackArguments,m
                     scope.throw_exception(msg.into());
                 }
             };
-
         }
         Err(e) => {
             let msg = v8::String::new(scope, &e.to_string()).unwrap();
             scope.throw_exception(msg.into());
         }
-    }    
+    }
 }
--- src/kstd.rs
+++ src/kstd.rs
@@ -6,50 +6,65 @@ fn core_print(
     func_callback_args: &v8::FunctionCallbackArguments,
 ) -> String {
     let mut acc: Vec<String> = vec![];
-    
+
     for i in 0..func_callback_args.length() {
         let arg = func_callback_args.get(i);
-        if arg.is_object(){
+        if arg.is_object() {
             let a_obj = arg.to_object(scope).unwrap().into();
-            if let Some(stringified) =  v8::json::stringify(scope, a_obj){
-                    acc.push(stringified.to_rust_string_lossy(scope))
+            if let Some(stringified) = v8::json::stringify(scope, a_obj) {
+                acc.push(stringified.to_rust_string_lossy(scope))
             }
-        }else{
+        } else {
             acc.push(arg.to_string(scope).unwrap().to_rust_string_lossy(scope))
-
-        }   
+        }
     }
     acc.join(" ")
 }
 
-pub fn print(scope: &mut v8::HandleScope,args: v8::FunctionCallbackArguments,_ret: v8::ReturnValue,) {
-
+pub fn print(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    _ret: v8::ReturnValue,
+) {
     let formatted = core_print(scope, &args);
     print!("{}", formatted);
-
 }
-pub fn println(scope: &mut v8::HandleScope,args: v8::FunctionCallbackArguments,_ret: v8::ReturnValue,) {
+pub fn println(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    _ret: v8::ReturnValue,
+) {
     let formatted = core_print(scope, &args);
     println!("{}", formatted);
-
 }
 
-pub fn assert(scope: &mut v8::HandleScope,args: v8::FunctionCallbackArguments,_ret: v8::ReturnValue,){
-    if args.get(0) == v8::Boolean::new(scope, false){
-        if args.length() >= 2{ //Todo, find out why this needs to be 2.
-            println!("Assertion failed: {}", args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope))
-        }else{
+pub fn assert(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    _ret: v8::ReturnValue,
+) {
+    if args.get(0) == v8::Boolean::new(scope, false) {
+        if args.length() >= 2 {
+            //Todo, find out why this needs to be 2.
+            println!(
+                "Assertion failed: {}",
+                args.get(1)
+                    .to_string(scope)
+                    .unwrap()
+                    .to_rust_string_lossy(scope)
+            )
+        } else {
             println!("Assertion failed");
         }
     }
 }
 // pub fn assert_or_panic(scope: &mut v8::HandleScope,args: v8::FunctionCallbackArguments,_ret: v8::ReturnValue,){
-    // if args.get(0) == v8::Boolean::new(scope, false){
-        // if args.length() >= 2{ //Todo, find out why this needs to be 2.
-        //     panic!("Assertion failed: {}", args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope))
-        // }else{
-        //     panic!("Assertion failed");
-        // }
-    // }
-    // assert_eq!(args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope), args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope))
+// if args.get(0) == v8::Boolean::new(scope, false){
+// if args.length() >= 2{ //Todo, find out why this needs to be 2.
+//     panic!("Assertion failed: {}", args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope))
+// }else{
+//     panic!("Assertion failed");
+// }
+// }
+// assert_eq!(args.get(0).to_string(scope).unwrap().to_rust_string_lossy(scope), args.get(1).to_string(scope).unwrap().to_rust_string_lossy(scope))
 // }
